a92c1352950edee603131ea529aef71069daa7cc
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2020-09-07 11:26:57 -0300

    feat(cdk): add basic working stack

--- myiam-py/tests/ddbs_utils.py
+++ myiam-py/tests/ddbs_utils.py
@@ -1,5 +1,6 @@
 
 def get_new_events(table, dynamodbstreams, iterator=None):
+    # NOTE: This function will be moved to whatever project hosts the stream lambdas.
     # NOTE: this is a simplified event-fetching routine to support local tests.
     if not iterator:
         dynamodbstreams.describe_stream(StreamArn=table.latest_stream_arn)
